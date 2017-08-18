4b2424e4d7060107f78e69040914fdf304a0e512
Plinio Prado <plinio.prado@immaginare.com.br>
2017-08-18 01:00:38 -0700

    loading inventory

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -8,16 +8,75 @@ describe('app', () => {
       expect(received).toBe(expected)
     })
 
-    it('should print inventory', () => {
-      app.setDb()
+    it('should reset and print empty inventory', () => {
+      app.resetDb()
       const received = app.printInventory();
-      expect(received).toEqual([])
+      const expected = {
+        code: 200,
+        data: {
+        '0': { name: 'item1', qt: 0 },
+        '1': { name: 'item2', qt: 0 },
+        '2': { name: 'item3', qt: 0 },
+        '3': { name: 'item4', qt: 0 },
+        '4': { name: 'item5', qt: 0 }
+        }
+      }
+      expect(received).toEqual(expected)
     })
 
-    describe('should refill inventory', () => {
+    describe('Refill inventory', () => {
+
+      const mockData = [
+        { id: '0', qt: 10 },
+        { id: '1', qt: 20 },
+        { id: '2', qt: 30 }
+      ]
+
+      it('should not load number', () => {
+        const received = app.refillInventory(1);
+        expect(received.code).toBe(400)
+      })
       
+      it('should load array', () => {
+        app.resetDb()
+        const received = app.refillInventory(mockData)
+        expect(received.code).toBe(200)
+      })  
 
+      it('load should be added in empty inventory', () => {
+        const received = app.printInventory()
+        const expected = {
+          code: 200,
+          data: {
+          '0': { name: 'item1', qt: 10 },
+          '1': { name: 'item2', qt: 20 },
+          '2': { name: 'item3', qt: 30 },
+          '3': { name: 'item4', qt: 0 },
+          '4': { name: 'item5', qt: 0 }
+          }
+        }
+        expect(received).toEqual(expected)
+      })
+      
+      it('load should be added in loaded inventory', () => {
+        app.refillInventory(mockData)
+        const received = app.printInventory()
+        const expected = {
+          code: 200,
+          data: {
+          '0': { name: 'item1', qt: 20 },
+          '1': { name: 'item2', qt: 40 },
+          '2': { name: 'item3', qt: 60 },
+          '3': { name: 'item4', qt: 0 },
+          '4': { name: 'item5', qt: 0 }
+          }
+        }
+        expect(received).toEqual(expected)
+      })  
+    
     })
+
+
 
     describe('should re-supply change', () => {
 
