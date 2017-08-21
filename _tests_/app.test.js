28a97d916528e685e733424c8c663832fd6c2c0a
Plinio Prado <plinio.prado@immaginare.com.br>
2017-08-21 00:49:58 -0700

    working begfore cleaning return change code

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -11,7 +11,7 @@ describe('app', () => {
       products: {
       '0': { name: 'item1', price: 1.00, qt: 0 },
       '1': { name: 'item2', price: 1.25, qt: 0 },
-      '2': { name: 'item3', price: 0.65, qt: 0 },
+      '2': { name: 'item3', price: 0.66, qt: 0 },
       '3': { name: 'item4', price: 1.00, qt: 0 },
       '4': { name: 'item5', proce: 3.00, qt: 0 }
       }
@@ -64,7 +64,7 @@ describe('app', () => {
         products: {
           '0': { name: 'item1', price: 1.00, qt: 10 },
           '1': { name: 'item2', price: 1.25, qt: 20 },
-          '2': { name: 'item3', price: 0.65, qt: 30 },
+          '2': { name: 'item3', price: 0.66, qt: 30 },
           '3': { name: 'item4', price: 1.00, qt: 0 },
           '4': { name: 'item5', proce: 3.00, qt: 0 }
         }
@@ -85,7 +85,7 @@ describe('app', () => {
         products: {
           '0': { name: 'item1', price: 1.00, qt: 10 },
           '1': { name: 'item2', price: 1.25, qt: 20 },
-          '2': { name: 'item3', price: 0.65, qt: 40 },
+          '2': { name: 'item3', price: 0.66, qt: 40 },
           '3': { name: 'item4', price: 1.00, qt: 20 },
           '4': { name: 'item5', proce: 3.00, qt: 0 }
         }
@@ -101,61 +101,57 @@ describe('app', () => {
       expect(received.code).toBe(400)
     })
 
-    const mockCoins1 = {
-      '1': 7,
-      '5': 6,
-      '10': 5,
-      '25': 4,
-      '50': 3,
-      '100': 2,
-      '200': 1
-      }
-
     it('should load coin array', () => {
-      const received = app.resupplyChange(mockCoins1)
+      const received = app.resupplyChange({
+        '1': 4,
+        '5': 9,
+        '10': 9,
+        '25': 9,
+        '50': 9,
+        '100': 9,
+        '200': 9        
+      })
       expect(received).toEqual({ code: 200 })
     })
 
-    it('load should be added in empty cashier', () => {
+    it('load should be added to empty cashier', () => {
       const received = app.printCashier()
       const expected = {
         code: 200,
         coins: {
-          '1': 7,
-          '5': 6,
-          '10': 5,
-          '25': 4,
-          '50': 3,
-          '100': 2,
-          '200': 1
+          '1': 4,
+          '5': 9,
+          '10': 9,
+          '25': 9,
+          '50': 9,
+          '100': 9,
+          '200': 9
           }
       }
       expect(received).toEqual(expected)
     })
 
-    const mockCoins2 = {
-      '1': 1,
-      '5': 1,
-      '10': 1,
-      '25': 1,
-      '50': 1,
-      '100': 1,
-      '200': 1
-      }
-
     it('load should be added in loaded cashier', () => {
-      app.resupplyChange(mockCoins2)
+      app.resupplyChange({
+        '1': 1,
+        '5': 1,
+        '10': 1,
+        '25': 1,
+        '50': 1,
+        '100': 1,
+        '200': 1        
+      })
       const received = app.printCashier()
       const expected = {
         code: 200,
         coins: {
-          '1': 8,
-          '5': 7,
-          '10': 6,
-          '25': 5,
-          '50': 4,
-          '100': 3,
-          '200': 2
+          '1': 5,
+          '5': 10,
+          '10': 10,
+          '25': 10,
+          '50': 10,
+          '100': 10,
+          '200': 10
           }
       }
       expect(received).toEqual(expected)
@@ -166,18 +162,16 @@ describe('app', () => {
 
   describe('sell', () => {
 
-    const mockSale = {
-      product: '1',
-      payment: {
-        '100': 1,
-        '50': 1
-      }
-    }
-
     describe('when dispense inventory', () => {
 
       it('should return ok if valid data', () => {
-        const received = app.saleDispense(mockSale)
+        const received = app.saleDispense({
+          product: '1',
+          payment: {
+            '100': 1,
+            '50': 1
+          }
+        })
         expect(received).toEqual({ code: 200 })
       })
 
@@ -221,13 +215,65 @@ describe('app', () => {
         expect(received.code).toEqual(400)
       })
 
+      it('cashier should not be impacted by invalid sales', () => {
+        const received = app.printCashier()
+        const expected = {
+          code: 200,
+          coins: {
+            '1': 5,
+            '5': 10,
+            '10': 10,
+            '25': 10,
+            '50': 10,
+            '100': 10,
+            '200': 10
+            }
+        }
+        expect(received).toEqual(expected)
+      })
     })
 
     describe('when return change', () => {
       
-      it('should return ok if valid data', () => {
-        const received = app.saleReturnChange(mockSale)
-        expect(received).toEqual({ code: 200 })
+      it('should return change if valid data', () => {
+        const received = app.saleReturnChange(
+          {
+            product: '2',
+            payment: {
+              '100': 1
+            }
+          }
+        )
+        expect(received).toEqual({ code: 200, change: { '25': 1, '5': 1, '1': 4 } })
+      })
+
+      it('cashier should reflect received and returned coins', () => {   
+        const received = app.printCashier()
+        const expected = {
+          code: 200,
+          coins: {
+            '1': 1,
+            '5': 9,
+            '10': 10,
+            '25': 9,
+            '50': 10,
+            '100': 11,
+            '200': 10
+            }
+        }
+        expect(received).toEqual(expected)
+      })   
+
+      it('should return change limited to available coins, confirming if last change updated cashier', () => {
+        const received = app.saleReturnChange(
+          {
+            product: '2',
+            payment: {
+              '100': 1
+            }
+          }
+        )
+        expect(received).toEqual({ code: 200, change: { '25': 1, '5': 1, '1': 1 } })
       })
 
     })
